4d1fa7327cd650548123fdaecfab9fb02fe58c89
Medine Yavuz <medineyldr.21@gmail.com>
2024-11-24 12:58:37 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,235 @@
+// veriler
+
+const starwarsApi={
+    "characters": [
+      {
+        "id": 1,
+        "name": "Luke Skywalker",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg",
+        "homeworld": "tatooine"
+      },
+      {
+        "id": 2,
+        "name": "C-3PO",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/3/3f/C-3PO_TLJ_Card_Trader_Award_Card.png",
+        "homeworld": "tatooine"
+      },
+      {
+        "id": 3,
+        "name": "R2-D2",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/e/eb/ArtooTFA2-Fathead.png",
+        "homeworld": "naboo"
+      },
+      {
+        "id": 4,
+        "name": "Darth Vader",
+        "pic": "https://vignette.wikia.nocookie.net/fr.starwars/images/3/32/Dark_Vador.jpg",
+        "homeworld": "tatooine"
+      },
+      {
+        "id": 5,
+        "name": "Leia Organa",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/f/fc/Leia_Organa_TLJ.png",
+        "homeworld": "alderaan"
+      },
+      {
+        "id": 6,
+        "name": "Owen Lars",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/e/eb/OwenCardTrader.png",
+        "homeworld": "tatooine"
+      },
+      {
+        "id": 7,
+        "name": "Beru Whitesun lars",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/c/cc/BeruCardTrader.png",
+        "homeworld": "tatooine"
+      },
+      {
+        "id": 8,
+        "name": "R5-D4",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/c/cb/R5-D4_Sideshow.png",
+        "homeworld": "tatooine"
+      },
+      {
+        "id": 9,
+        "name": "Biggs Darklighter",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/0/00/BiggsHS-ANH.png",
+        "homeworld": "tatooine"
+      },
+      {
+        "id": 10,
+        "name": "Obi-Wan Kenobi",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/4/4e/ObiWanHS-SWE.jpg",
+        "homeworld": "stewjon"
+      },
+      {
+        "id": 11,
+        "name": "Anakin Skywalker",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/6/6f/Anakin_Skywalker_RotS.png",
+        "homeworld": "tatooine"
+      },
+      {
+        "id": 12,
+        "name": "Wilhuff Tarkin",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/c/c1/Tarkininfobox.jpg",
+        "homeworld": "eriadu"
+      },
+      {
+        "id": 13,
+        "name": "Chewbacca",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/4/48/Chewbacca_TLJ.png",
+        "homeworld": "kashyyyk"
+      },
+      {
+        "id": 14,
+        "name": "Han Solo",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/e/e2/TFAHanSolo.png",
+        "homeworld": "corellia"
+      },
+      {
+        "id": 15,
+        "name": "Greedo",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/c/c6/Greedo.jpg",
+        "homeworld": "Rodia"
+      },
+      {
+        "id": 16,
+        "name": "Jabba Desilijic Tiure",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/7/7f/Jabba_SWSB.png",
+        "homeworld": "tatooine"
+      },
+      {
+        "id": 18,
+        "name": "Wedge Antilles",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/6/60/WedgeHelmetless-ROTJHD.jpg",
+        "homeworld": "corellia"
+      },
+      {
+        "id": 19,
+        "name": "Jek Tono Porkins",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/e/eb/JekPorkins-DB.png",
+        "homeworld": "bestine"
+      },
+      {
+        "id": 20,
+        "name": "Yoda",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/d/d6/Yoda_SWSB.png",
+         "homeworld": "other"
+      },
+      {
+        "id": 21,
+        "name": "Palpatine",
+        "pic": "https://vignette.wikia.nocookie.net/starwars/images/d/d8/Emperor_Sidious.png",
+        "homeworld": "naboo"
+      }
+    ]
+  };
+
+  // FİLTRELEME:
+
+
+
+  let filteredHomeworld = null; // Seçilen homeworld burada saklanacak
+
+  function createRadioInputs() {
+      const homeworldsRaw = [
+          ...new Set(
+              starwarsApi.characters
+                  .map(character => character.homeworld?.toLowerCase() || "unknown")
+          )
+      ];
+  
+      const form = document.createElement("form");
+      form.id = "homeworlds-form";
+  
+      homeworldsRaw.forEach((homeworld, index) => {
+          const label = document.createElement("label");
+          label.for = `homeworld-${index}`;
+          label.textContent = homeworld;
+  
+          const radio = document.createElement("input");
+          radio.type = "radio";
+          radio.name = "homeworld";
+          radio.id = `homeworld-${index}`;
+          radio.value = homeworld;
+  
+          // Event listener ile değişkeni güncelle
+          radio.addEventListener("change", () => {
+              filteredHomeworld = radio.value;
+              renderCharacters(); // Seçime göre kartları yeniden oluştur
+          });
+  
+          form.appendChild(radio);
+          form.appendChild(label);
+          form.appendChild(document.createElement("br"));
+      });
+  
+      document.body.appendChild(form);
+  }
+  
+  // Radio inputları oluştur
+  createRadioInputs();
+  
+
+
+  
+  
+
+
+  
+  
+ 
+
+// oluşturulacak div için değişkeni global tanımladım.
+
+  let newCharacter;
+
+
+//   butona basıldığında karakterleri render edecek fonksiyon
+
+ function renderCharacters(){
+    if(filteredHomeworld!=null){
+      starwarsApi.characters.forEach(character => {
+        character.filter 
+      })
+    }
+
+    newCharacter=document.createElement("div");
+    newCharacter.className = "row";
+
+
+// array üzerinde forEach ile döngü oluşturdum.
+
+    starwarsApi.characters.forEach(character => {
+        const renderString = `
+        <div class="card" style="width: 18rem; margin: 10px;">
+            <img src="${character.pic}" class="card-img-top" alt="${character.name}">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${character.id}</li>
+                <li class="list-group-item">Name: ${character.name}</li>
+                <li class="list-group-item">Homeworld: ${character.homeworld || 'Unknown'}</li>
+            </ul>
+        </div>`;
+
+        const tempDiv = document.createElement("div");
+        tempDiv.innerHTML = renderString.trim();
+        newCharacter.appendChild(tempDiv.firstChild); // Kartı eklemek için
+    });
+    
+     // Oluşturulan kartları sayfaya eklemek için:
+
+    document.body.appendChild(newCharacter);
+ }
+
+
+  // Oluşturulan kartları sayfadan kaldırmak için:
+ function removeCharacters() {
+        newCharacter.remove();   
+}
+
+
+
+
+
+
+  
